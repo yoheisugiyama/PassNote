3ae90832f9b64d35d710b1dd7be7ee887b3b05c3
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2015-03-22 18:28:58 +0900

    another commit

--- index.php
+++ index.php
@@ -29,6 +29,9 @@ define('ROOT', dirname(__FILE__));
 define('WEBROOT_DIR', 'webroot');
 define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
 
+// hello
+
+
 /**
  * This only needs to be changed if the "cake" directory is located
  * outside of the distributed structure.
